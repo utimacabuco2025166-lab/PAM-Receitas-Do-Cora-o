9f22076705293a2195447cf3e6c3141acb259fc4
Yasmin Dias <yasmin.cruz.2025072@my.istec.pt>
2026-07-17 16:51:56 +0100

    Update favorites_notifier.dart

--- cookbook_app/lib/features/recipes/presentation/providers/favorites_notifier.dart
+++ cookbook_app/lib/features/recipes/presentation/providers/favorites_notifier.dart
@@ -10,7 +10,7 @@ class FavoritesNotifier extends _$FavoritesNotifier {
 
   @override
   FavoritesState build() {
-    _loadFavorites();
+    _loadFavorites(); 
     return FavoritesState.initial();
   }
 
